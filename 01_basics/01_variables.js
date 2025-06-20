dfa57829d1982fad6e5075c044548e1cb9d53381
aditya0814raj <adityaraj8559@gmail.com>
2025-06-20 11:30:18 +0530

    variables.done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 11111
+let accountEmail = "aditya@0814"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 22222  
+console.log(accountId)
+
+accountEmail = "xyz@123"
+accountPassword = "54321"
+accountCity = "Bihar"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
