4f38606c9e3e519d50b96b08fe23dd83db83dff5
aditya0814raj <adityaraj8559@gmail.com>
2025-06-21 12:11:08 +0530

    datatypes.summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+//primitive datatypes
+
+// 7 types : String, Number, Boolean,null,undefined,Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log ( id === anotherId)
+
+const bigNumber = 6258534578535383353253n // n shows that given datatypes os if BigInt
+console.log ( bigNumber )
+
+
+
+// Reference types ( non primitive )
+
+// Array,Objects,Functions
+
+const heros = ["shaktiman","heman","doga"];
+let myObj = { 
+    name : "Aditya",
+    age : 22,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof anotherId )  //check all the variables's datatypes
+
+
+ // note : read typeof variable from ecma 
