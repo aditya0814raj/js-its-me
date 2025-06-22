64961b90dbb9429f79e5ff61af6e988782953c39
aditya0814raj <adityaraj8559@gmail.com>
2025-06-22 22:46:54 +0530

    num and math.done

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,29 @@
+const score = 100  // by this way it is not confiremed that our datatypes is numnber or else other.
+console.log(score)
+
+const balance = new Number (100.032999) // here by this way now it is confirmed that 100 stored in number datatypes
+console.log(balance)
+
+console.log(balance.toString()) //here 100 is string 
+console.log(balance.toString().length) // as after applying (.tostring) it become a string we can apply here string properties.
+console.log(balance.toFixed(3)) // (.tofixed) used to fixed no. of numbers after decimal
+
+const otherNumber = 23.8954654
+console.log(otherNumber.toPrecision(1)) // read and practice it via online 
+
+const hundreds = 10000000000000
+console.log(hundreds.toLocaleString())
+
+
+//+++++++++++++++++++++++++++++        MATHS        +++++++++++++++++++++++++++++++++++++++++++++++++++//
+console.log(Math)
+console.log(Math.)
+console.log(Math.)
+console.log(Math.)
+console.log(Math.)
+console.log(Math.)
+console.log(Math.)
+
+
+
+
