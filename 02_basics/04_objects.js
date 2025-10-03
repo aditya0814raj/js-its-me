b8dfb2f1939ae8f6fbb11f3acd6235cf446aeb7d
aditya0814raj <adityaraj8559@gmail.com>
2025-10-04 04:55:34 +0530

    save changes

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -4,7 +4,7 @@ const tinderUser = {}                 //non-singleton object    but both will gi
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
-console.log(tinderUser)
+//console.log(tinderUser)
 
 //multiple nesting of objects  
 
@@ -19,15 +19,16 @@ const regularUser = {
     }
 
 }
-console.log(regularUser.fullname.userfullname.lastname)
+//console.log(regularUser.fullname.userfullname.lastname)
 
+//                          MERGING OF OBJECT
 const obj1 = {1:"a", 2: "b"}                     //go to console on google and 'const obj1 = {1:"a", 2: "b"}'  past it. then enter and again type 'obj1' then enter and then access all properties.              
 const obj2 = {3:"a", 4: "b"}
 const obj4 = {5:"a", 6: "b"}
-const obj3 = {obj1,obj2}
-//const obj3 = Object.assign({},obj1,obj2,obj4)      //{} it act as target here and remaining as source in ojbect.assign()
-//const obj3= {...obj1, ...obj2, ...obj4}
-console.log(obj3)
+//const obj3 = {obj1,obj2}                  //Here, obj1 and obj2 become nested objects, not merged.
+const obj3 = Object.assign({},obj1,obj2,obj4)      //{} it act as target here and remaining as source in ojbect.assign() {} → empty object, the target and obj1, obj2, obj4 → sources ,After this operation, obj3 will have all properties from obj1, obj2, obj4.
+//const obj3= {...obj1, ...obj2, ...obj4}          //This does the same thing as Object.assign({}, ...) and { ...obj } spreads all properties of the object into a new object.
+//console.log(obj3)
                         // ARRAY-OBJECT ----------------------
 const users = [
     {
